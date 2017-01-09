a9dca8ad67a7ba7c2b37475535e617aad9c96cbb
ycLo <wdo564211@gmail.com>
2017-01-09 14:29:46 +0800

    2 loginAct Edit by ycLo

--- app/src/main/java/yclo/ntoufoodmap/LoginActivity.java
+++ app/src/main/java/yclo/ntoufoodmap/LoginActivity.java
@@ -1,11 +1,10 @@
 package yclo.ntoufoodmap;
 
-import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.StrictMode;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -20,7 +19,6 @@ public class LoginActivity extends AppCompatActivity {
     private TextView user = null;
     private TextView pwd = null;
     private Gson gson = new Gson();
-    private AlertDialog.Builder dialog = new AlertDialog.Builder(LoginActivity.this);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -46,6 +44,7 @@ public class LoginActivity extends AppCompatActivity {
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                AlertDialog.Builder dialog = new AlertDialog.Builder(LoginActivity.this);
                 String response = "";
                 try {
                     response = ConnectAPI.sendPost("API/login.php", "user=" + user.getText().toString() + "&pwd=" + pwd.getText().toString());
