f613ad5595c8bc0d7fb4c33ef4f1bc29e8d76712
ycLo <wdo564211@gmail.com>
2017-01-11 02:19:47 +0800

    gg

--- app/src/main/java/yclo/ntoufoodmap/ConnectAPI.java
+++ app/src/main/java/yclo/ntoufoodmap/ConnectAPI.java
@@ -1,12 +1,19 @@
 package yclo.ntoufoodmap;
 
+import android.util.Log;
+
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.text.ParseException;
 
 import javax.net.ssl.HttpsURLConnection;
 
@@ -57,6 +64,7 @@ public class ConnectAPI{
         }
         in.close();
 
+        con.disconnect();
         //print result
         return  response.toString();
 
